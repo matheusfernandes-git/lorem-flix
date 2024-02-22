0b7a71e75b9b31b332975e2378fb686aea623b6a
Matheus Fernandes <matheusfernandes311@hotmail.com>
2024-02-21 22:27:55 -0300

    adding new features

--- src/App.js
+++ src/App.js
@@ -1,103 +1,113 @@
-import { useState } from 'react';
-import Banner from './components/Banner/Banner';
-import Footer from './components/Footer/Fotter';
-import Form from './components/Form/Form';
-import MoviesList from './components/MoviesList/MoviesList';
-import { v4 as uuidv4 } from 'uuid';  /*Importando um id aleatório pra passar nas keys*/
+import { useEffect, useState } from "react";
+import Banner from "./components/Banner/Banner";
+import Footer from "./components/Footer/Fotter";
+import Form from "./components/Form/Form";
+import MoviesList from "./components/MoviesList/MoviesList";
+import { v4 as uuidv4 } from "uuid";
+import {
+  deleteFromLocalStorage,
+  getFromLocalStorage,
+  saveToLocalStorage,
+} from "./components/utils/storage";
 
 function App() {
-
   const [moviesCard, setMoviesCard] = useState([
     {
       id: uuidv4(),
-      name: 'Terror',
-      color: '#FF4858'
+      name: "Terror",
+      color: "#FF4858",
     },
     {
       id: uuidv4(),
-      name: 'Comédia',
-      color: '#FFF5D9'
+      name: "Comédia",
+      color: "#FFF5D9",
     },
     {
       id: uuidv4(),
-      name: 'Romance',
-      color: '#FDE7E8'
+      name: "Romance",
+      color: "#FDE7E8",
     },
     {
       id: uuidv4(),
-      name: 'Ação',
-      color: '#CACACA'
+      name: "Ação",
+      color: "#CACACA",
     },
     {
       id: uuidv4(),
-      name: 'Aventura',
-      color: '#D9F7E9'
+      name: "Aventura",
+      color: "#D9F7E9",
     },
     {
       id: uuidv4(),
-      name: 'Drama',
-      color: '#FFF5D9'
+      name: "Drama",
+      color: "#FFF5D9",
     },
     {
       id: uuidv4(),
-      name: 'Ficção científica',
-      color: '#FFEEDF'
-    }
-  ])
+      name: "Ficção científica",
+      color: "#FFEEDF",
+    },
+  ]);
+  const [movies, setMovies] = useState([]);
 
-  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    setMovies(getFromLocalStorage("movies"));
+    setMoviesCard(getFromLocalStorage("card"));
+  }, []);
 
-  const addNewMovie = (movie) => {
-    debugger
-    setMovies([...movies, movie])
-  }
+  const addNewMovie = (newMovie) => {
+    //debugger;
+    const updatedMovies = [...movies, newMovie];
+    setMovies(updatedMovies);
+    saveToLocalStorage("movies", updatedMovies);
+  };
 
-  // function deleteMovie(id){
-  //   setMovies(movies.filter(movie => movie.id !== id));
-  // }
-
-  function changeMovieColor(color, name){
-    setMoviesCard(moviesCard.map(card => {
-      if(card.name === name){      /*O certo é passar o id aleatório.*/
+  const changeMovieColor = (color, name) => {
+    const filteredMoviesColor = moviesCard.map((card) => {
+      if (card.name === name) {
         card.color = color;
       }
       return card;
-    }))
-  }
+    });
+    setMoviesCard(filteredMoviesColor);
+    saveToLocalStorage("card", filteredMoviesColor);
+  };
 
-  // function registerGender(newGender){
-  //   setMoviesCard([...moviesCard, {...newGender, id: uuidv4()}])
-  // }
+  const deleteMovie = (id) => {
+    const filteredMovies = movies.filter((movie) => movie.id !== id);
+    setMovies(filteredMovies);
+    deleteFromLocalStorage();
+    saveToLocalStorage("movies", filteredMovies);
+  };
 
-  // function solveFavorites(id) {
-  //   setMovies(movies.map(movie => {
-  //     if(movie.id === id) movie.favorito = !movie.favorito;
-  //     return movie;
-  //   }))
-  // }
+  const handleFavorite = (id) => {
+    const filteredMovies = movies.map((movie) => {
+      if (movie.id === id) movie.favorite = !movie.favorite;
+      return movie;
+    });
+    setMovies(filteredMovies);
+    saveToLocalStorage("movies", filteredMovies);
+  };
 
   return (
     <div className="App">
-      <Banner/>
-      <Form 
-        // registerGender={registerGender}
-        moviesName={moviesCard.map(card => card.name)} 
-        whenRegistered={movie => addNewMovie(movie)}
+      <Banner />
+      <Form
+        moviesName={moviesCard.map((card) => card.name)}
+        whenRegistered={(movie) => addNewMovie(movie)}
       />
-
-      {moviesCard.map(card => 
+      {moviesCard.map((card) => (
         <MoviesList
-          // whenFavorite={solveFavorites}
           changeColor={changeMovieColor}
-          key={card.name} 
-          name={card.name} 
-          color={card.color} 
-          movie={movies.filter(movie => movie.gender === card.name)}
-          // whenDelete={deleteMovie}
+          key={card.name}
+          name={card.name}
+          color={card.color}
+          movie={movies.filter((movie) => movie.gender === card.name)}
+          whenFavorite={handleFavorite}
+          whenDelete={deleteMovie}
         />
-      )}   
-
-      <Footer/>
+      ))}
+      <Footer />
     </div>
   );
 }
